9cb2711f12acd91c0f9d5f8035cb984139f7fc09
Nate Sigmon <sigmonate@gmail.com>
2021-04-08 14:29:57 -0700

    created this

---
+++ r/speedoflight.R
@@ -0,0 +1,8 @@
+c <- 299792458
+lightMinute <- c*60
+lightHour <- lightMinute*60
+lightDay <- lightHour*24
+lightYear <- lightDay*365.2422
+cat("length of light year:", lightYear, "\n")
+cat ("that's too long", "\n")
+cat("length of light minute:", lightMinute)
